dc473154b068299d3bde9c7319e7e918fe3f7d46
Mark P Byrne <3760653+markbyrne@users.noreply.github.com>
2022-09-19 07:54:25 -0500

    v1.2, All AD PDFs saved

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # US Military Pay Scales
 ## Annualized US Military Pay Scales
-
-### This dataset compiles historical US Military pay scales, broken down by rank, time in service, and year.
+This dataset compiles historical US Military pay scales, broken down by rank, time in service, and year. Dataset includes historical data from 1949-Present for Active Duty military pay. Includes Ranks E-1 through O-10, and from <2 Years in Service to 40 years in service.
 
 ### Live datasets found in datasets/
-Annual Pay Scales show every rank for a given year. 
-Rank Pay by Year shows a given rank over every year
+Annual Pay Scales show every rank for a given year.
 
-### Future buildout will include pay scales back to 1949
+### Future buildout will include active and reserve pay scales back to 1949
+
+### Raw Data PDFs can be found in milpayscraper/files.
--- milpayscraper/__init__.py
+++ milpayscraper/__init__.py
@@ -6,14 +6,12 @@ __version__ = "1.0"
 __email__ = "MarkByrne.pro+milpayscraper@gmail.com"
 __date__ = "20220918"
 
+from datetime import datetime
 from bs4 import BeautifulSoup as bs
 import requests
 import os
-import tabula as tb
-import pandas as pd
 
-
-url = "https://www.military.com/benefits/military-pay/charts"
+url = "https://www.military.com/benefits/military-pay/charts/historical-military-pay-rates.html"
 
 ad_dir = "files/active_duty"
 ad_files_list = os.listdir(ad_dir)
@@ -21,18 +19,43 @@ datasets_dir = "../datasets"
 
 
 def get_AD_pay_charts():
+    print("Downloading Pay Charts")
     response = requests.get(url)
     soup = bs(response.text, 'html.parser')
-    content = soup.find('div', {'class': 'page__content'})
+    content = soup.find('div', {'id': 'bodyContent'})
+    if content is None:
+        raise Exception("Unable to find Body Content.")
 
-    for link in content.find_all('a'):
-        if link.string and "Active Duty Pay Charts" in link.string:
-            year = link.string[0:4]
+    pdf_table = None
+    for table in content.find_all('table'):
+        print(table)
+        print(table.th.string)
+        if table.th.string == "1949-1979":
+            pdf_table = table
+            print("PDF Table Found")
+            break
+
+    if pdf_table is None:
+        raise Exception("Unable to find PDF Table.")
+
+    for link in pdf_table.find_all('a'):
+        if link.string:
+            date = None
+            try:
+                date = datetime.strptime(link.string.strip(), "%b. %d, %Y")
+            except ValueError:
+                try:
+                    date = datetime.strptime(link.string.strip(), "%b %d, %Y")
+                except ValueError:
+                    date = datetime.strptime(link.string.strip(), "%b .%d, %Y")
+            year = datetime.strftime(date, "%Y")
+            month = datetime.strftime(date, "%m")
+            day = datetime.strftime(date, "%d")
             file_url = link['href']
 
-            if f"AD-{year}.pdf" not in ad_files_list:
-                print(f"Downloading -> AD-{year}.pdf from {file_url}...")
-                download_pdf(file_url, f"{ad_dir}/AD-{year}.pdf")
+            if f"AD-{year}-{month}-{day}.pdf" not in ad_files_list:
+                print(f"Downloading -> AD-{year}-{month}-{day}.pdf from {file_url}...")
+                download_pdf(file_url, f"{ad_dir}/AD-{year}-{month}-{day}.pdf")
                 print(f"Complete.\n")
 
 
@@ -49,11 +72,15 @@ def download_pdf(url, file_name):
 
 
 def scrape_pdf(file_name):
+    import tabula as tb
+    import pandas as pd
+    import numpy as np
+
     data = tb.read_pdf(file_name, pages='1', pandas_options={'header': None})
     data_df = pd.DataFrame(data=data[0])
 
     print(data_df)
-    if data_df.iloc[0,0] == "O-10":
+    if data_df.iloc[0, 0] == "O-10":
         data_df.set_axis(["Rank","2 or less","Over 2","Over 3","Over 4","Over 6","Over 8","Over 10","Over 12","Over 14","Over 16","Over 18","Over 20","Over 22","Over 24","Over 26","Over 28","Over 30","Over 32","Over 34","Over 36","Over 38","Over 40"], axis=1, inplace=True)
     else:
         data_df.rename(columns=data_df.iloc[0], inplace=True)
@@ -63,11 +90,17 @@ def scrape_pdf(file_name):
     data_df = data_df[data_df['Rank'].notna()]
     data_df.set_index("Rank", inplace=True)
 
+    data_df.replace(0.00, np.nan, inplace=True)
+    data_df.replace('0.00', np.nan, inplace=True)
+    data_df.replace('0.00 0.00', np.nan, inplace=True)
+
+    data_df[:] = data_df[:].replace("[$,]", "", regex=True).astype(float)
+
     print(data_df.head())
     data_df.to_csv(f'{datasets_dir}/annual_pay_scales/{os.path.basename(file_name)[:-4]}.csv', index=True)
 
 
-#get_AD_pay_charts()
-# for file in ad_files_list:
-#     scrape_pdf(f"{ad_dir}/{file}")
-scrape_pdf(f"files/active_duty/AD-2018.pdf")
+# get_AD_pay_charts()
+for file in ad_files_list:
+    scrape_pdf(f"{ad_dir}/{file}")
+# scrape_pdf(f"files/active_duty/AD-2018.pdf")
